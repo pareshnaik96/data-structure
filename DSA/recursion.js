077a4dd8e066b5f6552b6e4cce9d86ebb9430bb1
pareshnaik96 <pareshnaik678@gmail.com>
2022-07-06 12:57:06 +0530

    factorial, power using recursion, reverse and rotate

--- DSA/recursion.js
+++ DSA/recursion.js
@@ -1,8 +1,12 @@
+//find factorial of a number using recursion
+
 function fact(n){
     if( n ==1 ) return 1
     return n*fact(n-1)
 }
 console.log(fact(6))
+
+//find power of number using recursion
 
 function pow(b,exp){
     if(exp ==0) return 1
@@ -10,18 +14,17 @@ function pow(b,exp){
 }
 console.log(pow(2,5))
 
-
-
+//reverse the string and rotate last two index
 
 function reverse(str){
-    str=str.split("").reverse().join("")
+    str = str.split("").reverse().join("")
     return str
 }
-
+// console.log(reverse("aA bB cC dD"))
 function solution(str,n){
     let arr = str.split(" ")
     for(let i=0; i<arr.length; i++){
-        arr[i]=reverse(arr[i])
+        arr[i] = reverse(arr[i])
     }
     for(let i=0; i<arr.length; i+=n){
         let temp=arr[i]
